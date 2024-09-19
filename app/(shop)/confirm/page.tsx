fb8b9df15b2711a64d4e5d4c2b0bfd6b1f73784d
ayoubaitarab <ayoubaitarab20@gmail.com>
2024-09-19 18:17:27 +0200

    confirm page updated

--- app/(shop)/confirm/page.tsx
+++ app/(shop)/confirm/page.tsx
@@ -11,9 +11,6 @@ function PaymentConfirm() {
 				height={130}
 			/>
 			<h2 className='text-[24px]'>Payment Successful !</h2>
-			<h2 className='text-[17px] text-center mt-6 text-gray-500'>We sent an email with your
-				order confirmation
-				along with Digital Content</h2>
 			<Link
 				href="/"
 				className='p-2 mt-6 text-white rounded-md bg-primary'>
